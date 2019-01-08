a3961e519efd1e371e625eb6045d8bdaeb48eee1
Niall Riordan <nriordan.dev@gmail.com>
2019-01-08 12:01:42 +0100

    Add Book List and Book Details views

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from catalog.models import Book, Author, BookInstance, Genre
 
 def index(request):
@@ -25,3 +26,9 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
